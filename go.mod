d004e1ae4f871bf85779d61beb8d480ef2318e99
Brian Galeano <97926762+bgcodehub@users.noreply.github.com>
2025-01-29 17:44:24 -0500

    Make Artifactory URL configurable using GitHub Secrets

---
+++ go.mod
@@ -0,0 +1,7 @@
+module go-api
+
+go 1.20
+
+require (
+	github.com/stretchr/testify v1.8.1
+)
---
+++ tests/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHomeHandler(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+
+	homeHandler(w, req)
+
+	resp := w.Result()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK, got %v", resp.StatusCode)
+	}
+}
